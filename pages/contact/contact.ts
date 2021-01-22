571dded13f51dc250b0c2802a9cbef20901bc855
Zack-Mendiola <john.mendiola101420@my.lsgh.edu.ph>
2021-01-22 13:16:13 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -28,15 +28,15 @@ calculateIdealWeight(){
   this.overFive = this.totalHeight-60;
 
   if (this.gender == "m"){
-    this.idealWeightR = 52+(1.9*this.overFive);
-    this.idealWeightM = 56.2+(1.41*this.overFive);
-    this.idealWeightH = 48.0+(2.7*this.overFive);
-    this.idealWeightD = 50.0+(2.3*this.overFive);
+    this.idealWeightR = Math.round(52+(1.9*this.overFive));
+    this.idealWeightM = Math.round(56.2+(1.41*this.overFive));
+    this.idealWeightH = Math.round(48.0+(2.7*this.overFive));
+    this.idealWeightD = Math.round(50.0+(2.3*this.overFive));
   } else if (this.gender == "f"){
-    this.idealWeightR = 49+(1.7*this.overFive);
-    this.idealWeightM = 53.1+(1.36*this.overFive);
-    this.idealWeightH = 45.5+(2.2*this.overFive);
-    this.idealWeightD = 45.5+(2.3*this.overFive);
+    this.idealWeightR = Math.round(49+(1.7*this.overFive));
+    this.idealWeightM = Math.round(53.1+(1.36*this.overFive));
+    this.idealWeightH = Math.round(45.5+(2.2*this.overFive));
+    this.idealWeightD = Math.round(45.5+(2.3*this.overFive));
   }
 }
   
